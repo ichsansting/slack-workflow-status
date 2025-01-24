73a4b0ac222b8efb7a88ea1ac345b21abcaa7452
Ichsan Sting <ichsan.sting@traveloka.com>
2025-01-24 18:18:58 +0700

    Error on fail

--- src/main.ts
+++ src/main.ts
@@ -255,11 +255,11 @@ async function main(): Promise<void> {
     axios(config)
     .then(function (response) {
       console.log(JSON.stringify(response.data));
-      core.info(JSON.stringify(response.data));
+      core.setOutput('data', JSON.stringify(response.data) || '');
     })
     .catch(function (error) {
       console.error(JSON.stringify(error.response.data, null, 4));
-      core.info(JSON.stringify(error.response.data, null, 4));
+      core.setFailed(JSON.stringify(error.response.data, null, 4));
     });
 
   } catch (err) {
